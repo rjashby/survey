fa373d677a50c2dffaf62dfcd90baa2ad617ee60
Ryan Ashby <rjapsu160@gmail.com>
2022-01-12 15:52:41 -0800

    experiment with ways of capturing input values

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,9 @@ $(document).ready(function() {
     const firstName = $("#firstName").val();
     const lastName = $("#lastName").val();
     const favFood = $("#favFood").val();
-    const favMusic = $("input:radio[name=music]:checked").val();
+    const favMusic = $("input:radio:checked").val();
+    // const favMusic = $("input:radio[name=music]:checked").val();
+    // const favMusic = document.getElementById("survey-1").elements[3].value;
     const dob = $("#born").val();
     const favoriteColor = $("#color").val();
     const result = add(firstName, lastName, favFood, favMusic, dob, favoriteColor);
